b842cce3ed9bcd788a00f4e44a1193e43ab85877
Nham <nguyenlamhai89@gmail.com>
2023-09-13 17:45:10 +0700

    17:45

---
+++ Notes/screens/Homescreen.swift
@@ -0,0 +1,20 @@
+//
+//  Homescreen.swift
+//  Notes
+//
+//  Created by nham on 13/09/2023.
+//
+
+import SwiftUI
+
+struct Homescreen: View {
+    var body: some View {
+        Text("hef hef hef")
+    }
+}
+
+struct Homescreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Homescreen()
+    }
+}
